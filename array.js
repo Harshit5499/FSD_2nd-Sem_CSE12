1d81df2421da147ae47d6dd4361c42dc1982bbd3
Harshit Sahu <harshitgts777@gmail.com>
2026-04-17 09:20:45 +0530

    Arrays

--- array.js
+++ array.js
@@ -42,3 +42,11 @@
 
 // const mul = (a,b) => a*b;
 // console.log(mul(3,4));
+
+let num = [1,2,3,4,5];
+let filterevennum = num.filter(num =>num%2==0);
+console.log(filterevennum);
+
+let sum = [1,2,3,4,5];
+let total = sum.reduce((accumulator, currentValue)=> accumulator + currentValue,2);
+console.log(total);
